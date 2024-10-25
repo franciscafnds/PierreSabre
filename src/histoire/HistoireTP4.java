a4080564f05d4f3bb78902d1df06f40a34320f41
franciscafnds <francisca.fernandes.almeida@gmail.com>
2024-10-25 09:47:26 +0200

    tp4

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -7,5 +7,7 @@ public class HistoireTP4 {
 	public static void main(String[] args) {
 		Humain prof = new Humain("Prof", "kombucha", 54);
 		prof.direBonjour();
+		prof.boire();
+
 	}
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,15 +4,14 @@ public class Humain {
 	private String nom;
 	private String boissonPref;
 	private int argent;
-	
+
 	public Humain(String nom, String boissonPref, int argent) {
 		super();
 		this.nom = nom;
 		this.boissonPref = boissonPref;
 		this.argent = argent;
 	}
-	
-	
+
 	private String getNom() {
 		return nom;
 	}
@@ -21,32 +20,44 @@ public class Humain {
 		return argent;
 	}
 
-
 	public void direBonjour() {
 		System.out.println("Bonjour ! Je m'appelle " + getNom() + " et j'aime boire du " + boissonPref);
-		
+
 	}
-	
-	private void boire() {
+
+	public void boire() {
 		System.out.println("Mmmm, un bon verre de " + boissonPref + " ! GLOUPS !");
 	}
-	
-//	private void acheter(String bien, int prix) {
-//		
-//	}
-//	
-//	private void gagnerArgent(int gain) {
-//		
-//	}
-//	
-//	private void perdreArgent(int perte) {
-//		
-//	}
-//	
-//	private void parler(String texte) {
-//		
-//	}
-//	
-	
-	
+
+	private void acheter(String bien, int prix) {
+		int nvArgent = argent - prix;
+		if (nvArgent <= 0) {
+			System.out.println("Je n'ai plus que " + argent + " sous en poche. " + "Je ne peux même pas m'offrir un "
+					+ bien + " à " + prix + " sous.");
+		} else {
+			System.out.println("J'ai " + argent + " sous en poche. " + "Je vais pouvoir m'offrir " + bien + " à " + prix
+					+ " sous.");
+			perdreArgent(prix);
+
+		}
+	}
+
+	private void gagnerArgent(int gain) {
+		argent += gain;
+	}
+
+	private void perdreArgent(int perte) {
+		int nvArgent = argent - perte;
+		if (nvArgent < 0) {
+			argent = 0;
+		} else {
+			argent = nvArgent;
+		}
+		;
+	}
+
+	private void parler(String texte) {
+		System.out.println("<<" + texte + ">>");
+	}
+
 }
